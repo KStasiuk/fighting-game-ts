8c200841056428a840b050e3f7de749c20eca624
Konrad Stasiuk <konrad.stasiuk@venturelabs.team>
2022-03-31 23:24:44 +0200

    Added simple styles and emeny sprite

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,9 @@ import { Sprite } from '~/modules/Sprite';
 (() => {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   const player = new Sprite({ x: 123, y: 234 });
+  player.draw();
+
+  const enemy = new Sprite({ x: 0, y: 0 });
+  enemy.draw();
   console.log('player', player);
 })();
