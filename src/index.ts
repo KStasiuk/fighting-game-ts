eab8119c5de3d1ceda279f93317ae221ed34c73a
Konrad Stasiuk <konrad.stasiuk@venturelabs.team>
2022-03-31 23:54:17 +0200

    Added gravity to the players

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,29 @@ import { Sprite } from '~/modules/Sprite';
 
 (() => {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  const player = new Sprite({ x: 123, y: 234 });
-  player.draw();
+  const player = new Sprite({
+    position: { x: 123, y: 234 },
+    velocity: {
+      x: 0,
+      y: 10,
+    },
+    height: 100,
+  });
 
-  const enemy = new Sprite({ x: 0, y: 0 });
-  enemy.draw();
-  console.log('player', player);
+  const enemy = new Sprite({
+    position: { x: 0, y: 0 },
+    velocity: { x: 0, y: 0 },
+    height: 150,
+  });
+
+  function animate() {
+    window.requestAnimationFrame(animate);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    enemy.update();
+    console.log('animaton');
+  }
+
+  animate();
 })();
--- src/modules/Sprite.ts
+++ src/modules/Sprite.ts
@@ -1,14 +1,38 @@
-import { ctx } from '~/modules/context';
+import { canvas, ctx } from '~/modules/context';
+import { GRAVITY } from '~/modules/consts';
 
-export type SpritePosition = {
+export type Position = {
   x: number;
   y: number;
 };
+
+export type SpriteSettings = {
+  position: Position;
+  velocity: Position;
+  height: number;
+};
+
 export class Sprite {
-  constructor(public position: SpritePosition) {}
+  constructor(public settings: SpriteSettings) {}
 
   draw = () => {
     ctx.fillStyle = 'red';
-    ctx.fillRect(this.position.x, this.position.y, 50, 150);
+    ctx.fillRect(
+      this.settings.position.x,
+      this.settings.position.y,
+      50,
+      this.settings.height,
+    );
   };
+
+  update() {
+    this.draw();
+    const { position, velocity, height } = this.settings;
+    position.y += velocity.y;
+    if (position.y + height + velocity.y >= canvas.height) {
+      velocity.y = 0;
+    } else {
+      velocity.y += GRAVITY;
+    }
+  }
 }
